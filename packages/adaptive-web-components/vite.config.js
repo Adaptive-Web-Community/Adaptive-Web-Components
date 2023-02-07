e92b6ab580ee5f0b4d2710488946d1849dbe1d27
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-02-07 10:57:58 -0700

    add vite for bundling

---
+++ packages/adaptive-web-components/vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, "./src/index.ts"),
+			fileName: "adaptive-web-components.min.js",
+			formats: ["es"],
+		},
+		minify: "esbuild",
+		sourcemap: "inline",
+		outDir: "./dist",
+	},
+});
