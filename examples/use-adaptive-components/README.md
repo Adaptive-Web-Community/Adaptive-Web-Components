9f25834bb40e0dd9ee5ead062263066d0f2afd55
Brian Heston <47367562+bheston@users.noreply.github.com>
2023-02-10 11:08:37 -0800

    Slimmed down the doc commentary

--- examples/use-adaptive-components/README.md
+++ examples/use-adaptive-components/README.md
@@ -1,34 +1,13 @@
 # Using Adaptive Components
 
-Web components can be used in plain html with any framework just like native platform elements (button, input, etc.).
+Adaptive Web Components can be used in plain html with any framework, just like native platform elements (`button`, `input`, etc.).
+
+This example illustrates using the components in html with JavaScript to adjust Adaptive UI look and feel.
 
 ## Key features
 
 - Register components and use in plain html
-- Set component attributes and handle events in plain JavaScript
-- Set design token values to change the visual treatment
-
-## Details
-
-### Importing
-
-Unlike native platform elements, each component you want to use needs to be registered with the browser. Here we've imported
-all the necessary components except one to illustrate what that looks like. After you've run the sample, go to `index.ts` and
-uncomment the `text-area` import.
-
-### Light and dark mode
-
-Adaptive UI does light and dark mode differently than any other design system. This example contains the minimum setup to
-use the layering system that forms the basis of the adaptive color system.
-
-Because this is an html only example (and not your own web component for your app), you'll notice `background-color: var(--fill-color);`
-in the `html` selector in css. This applies the value from the `fillColor` design token to the page. In the setup code
-that token's default value is set to `layerFillFixedBase`, which is the primary (`Base`) level in the layering system.
-
-With the layering system setup, we can now change from light to dark mode by setting the `layerFillBaseLuminance` design token
-value. There are a couple constant values defined for convenience, but this value can be any decimal number between 0 and 1,
-indicating the luminosity of the `Base` layer.
-
-The last portion of this example illustrates adjusting the accent color. We've chosen three colors presented as radio options,
-but you can use any hex color value. Notice how the accent color adjusts in light or dark mode (or your own custom luminosity)
-to feel balanced and always meet contrast requirements.
+- Set component attributes and handle events
+- Adjust Adaptive UI for a variety of visual treatments
+- The color system uses recipes to apply color to components
+- The layering system forms the visual structure of your app, including applying the luminance value (light or dark modes)
--- examples/use-adaptive-components/src/index.ts
+++ examples/use-adaptive-components/src/index.ts
@@ -1,31 +1,36 @@
-// All components that are used in html need to be defined.
+// All components that are used in html must be defined.
 import "@adaptive-web/adaptive-web-components/avatar/define";
 import "@adaptive-web/adaptive-web-components/button/define";
 import "@adaptive-web/adaptive-web-components/card/define";
 import "@adaptive-web/adaptive-web-components/radio/define";
 import "@adaptive-web/adaptive-web-components/radio-group/define";
 import "@adaptive-web/adaptive-web-components/switch/define";
-// Uncomment to define `adaptive-text-field`.
+// Uncomment to define the `adaptive-text-field` element.
 // import "@adaptive-web/adaptive-web-components/text-field/define";
 
 import { accentBaseColor, fillColor, LayerBaseLuminance, layerFillBaseLuminance, layerFillFixedBase } from "@adaptive-web/adaptive-ui";
-import { DesignToken, FASTSwitch } from "@microsoft/fast-foundation";
+import { DesignToken, FASTRadioGroup, FASTSwitch } from "@microsoft/fast-foundation";
 
-// This must be called during initialization for the design tokens to be setup so the component styling is applied.
+// This must be called during initialization for the Design Tokens to be setup so the component styling is applied.
 DesignToken.registerDefaultStyleTarget();
 
+// The `fillColor` Design Token is the reference for many color recipes - setup the layering system as that basis.
+// Look at the css for how this displays in the browser in the plain html model.
 fillColor.withDefault(layerFillFixedBase);
 
-// Simple event handlers to change design token values
+// Simple event handlers to change Design Token values:
+
 document.getElementById("darkMode").onchange = function(event) {
     const checked = (event.target as FASTSwitch).checked;
-    console.log("darkMode onchange", checked);
+    console.log("darkMode change", checked);
+    // This Design Token causes `layerFillFixedBase` to update.
     // The constants are for convenience, but try changing them to another decimal value between 0 and 1.
     layerFillBaseLuminance.withDefault(checked ? LayerBaseLuminance.DarkMode : LayerBaseLuminance.LightMode);
 };
 
 document.getElementById("accentColor").onchange = function(event) {
-    const value = (event.target as FASTSwitch).value;
-    console.log("accentColor onchange", value);
+    const value = (event.target as FASTRadioGroup).value;
+    console.log("accentColor change", value);
+    // This Design Token causes the accent palette to update, including any components styled with recipes based on that palette.
     accentBaseColor.withDefault("#" + value);
 };
