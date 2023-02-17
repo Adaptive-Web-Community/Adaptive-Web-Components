af1e4b9a338afeca378f2b64c1f22ea0c85a8129
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-02-17 16:03:29 -0700

    update SB and remove unused interface

--- packages/adaptive-web-components/src/index.ts
+++ packages/adaptive-web-components/src/index.ts
@@ -1,16 +1,8 @@
-import type { DesignSystem, PartialDesignSystem } from "./design-system.js";
 import { DefaultDesignSystem } from "./design-system.js";
 
 export * from "./design-system.js";
 export * from "./components/index.js";
 export { AllComponents } from "./custom-elements.js";
-
-export interface AdaptiveDesignSystem {
-	designSystem: DesignSystem;
-	configureDesignSystem(options?: PartialDesignSystem, designSystem?: DesignSystem): this;
-	withPrefix(prefix: string): this;
-	defineAllComponents(registry?: CustomElementRegistry): void;
-}
 
 /**
  * The default DesignSystem.
