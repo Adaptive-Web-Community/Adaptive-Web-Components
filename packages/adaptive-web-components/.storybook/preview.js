f13c0f6e627d8fe61f26f1861b8f101d15bfbac1
Nathan Brown <nbrown414@outlook.com>
2023-02-22 11:42:18 -0700

    update storybook imports

--- packages/adaptive-web-components/.storybook/preview.js
+++ packages/adaptive-web-components/.storybook/preview.js
@@ -1,8 +1,8 @@
 import { DesignToken } from "@microsoft/fast-foundation";
 
-import { DefaultDesignSystem } from "../src/index";
+import { AdaptiveDesignSystem } from "../src/index";
 import { AllComponents } from "../src/custom-elements";
 
 DesignToken.registerDefaultStyleTarget();
 
-DefaultDesignSystem.defineComponents(AllComponents);
+AdaptiveDesignSystem.defineComponents(AllComponents);
