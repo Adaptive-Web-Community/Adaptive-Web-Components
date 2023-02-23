8d7d8d9c96a1175ede940e4bdd1497d83b6db90c
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-02-22 22:26:32 -0700

    revert my last PR (#41)

--- packages/adaptive-web-components/src/components/tooltip/tooltip.compose.ts
+++ packages/adaptive-web-components/src/components/tooltip/tooltip.compose.ts
@@ -1,18 +1,17 @@
+import { FASTTooltip } from '@microsoft/fast-foundation';
 import type { FASTElementDefinition } from '@microsoft/fast-element';
 import { componentBaseStyles } from "@adaptive-web/adaptive-ui";
 import type { ComposeOptions, DesignSystem } from "../../design-system.js";
 import { aestheticStyles, templateStyles } from "./tooltip.styles.js";
 import { template } from "./tooltip.template.js";
-import { AdaptiveTooltip } from './tooltip.js';
 
 const styles = [componentBaseStyles, templateStyles, aestheticStyles];
 
 export function composeTooltip(
     ds: DesignSystem,
-    options?: ComposeOptions<AdaptiveTooltip>
+    options?: ComposeOptions<FASTTooltip>
 ): FASTElementDefinition {
-    // TODO: switch back to FASTTooltip after https://github.com/microsoft/fast/pull/6649 is merged and published.
-    return AdaptiveTooltip.compose({
+    return FASTTooltip.compose({
         name: `${ds.prefix}-tooltip`,
         template: options?.template?.(ds) ?? template(ds),
         styles: options?.styles ?? styles,
