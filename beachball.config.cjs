976a6f3b157c603df09333136bcbd54cb0d8e830
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-02-23 12:01:45 -0700

    add beachball and api-extractor

---
+++ beachball.config.cjs
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+module.exports = {
+	disallowedChangeTypes: ["major"],
+	ignorePatterns: [
+		".ignore",
+		".github/",
+		".prettierrc",
+		".vscode/",
+		"jest..js",
+		"src/e2e/",
+		"src/tests/",
+		"src/fixtures/**",
+		// This one is especially important (otherwise dependabot would be blocked by change file requirements)
+		"package-lock.json",
+	]
+}
