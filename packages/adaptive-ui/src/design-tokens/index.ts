1189afd2004b30133a6fb7db39dbea199514da94
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-06-30 13:19:55 -0600

    exports sequence

--- packages/adaptive-ui/src/design-tokens/index.ts
+++ packages/adaptive-ui/src/design-tokens/index.ts
@@ -3,6 +3,6 @@ export * from "./color.js";
 export * from "./density.js";
 export * from "./elevation.js";
 export * from "./layer.js";
+export * from "./modules.js";
 export * from "./palette.js";
 export * from "./type.js";
-export * from "./modules.js";
