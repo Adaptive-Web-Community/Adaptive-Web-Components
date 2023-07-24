ee91c892acfff2609d624aeeea47d020cbf586bb
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2023-07-24 12:28:26 -0600

    working on new model for default components

--- packages/adaptive-web-components/src/components/checkbox/checkbox.compose.ts
+++ packages/adaptive-web-components/src/components/checkbox/checkbox.compose.ts
@@ -1,18 +1,29 @@
+import checkmarkIcon from "@fluentui/svg-icons/icons/checkmark_16_regular.svg";
+import subtractIcon from "@fluentui/svg-icons/icons/subtract_16_regular.svg";
 import { FASTCheckbox } from "@microsoft/fast-foundation";
 import type { ComposableStyles, FASTElementDefinition } from "@microsoft/fast-element";
 import { componentBaseStyles } from "@adaptive-web/adaptive-ui";
 import { ComposeOptions, DesignSystem } from "../../design-system.js";
 import { aestheticStyles, templateStyles } from "./checkbox.styles.js";
 import { CheckboxAnatomy, CheckboxStatics, template } from "./checkbox.template.js";
+import { styleModules } from "./checkbox.styles.modules.js";
 
 const defaultStyles = [componentBaseStyles, templateStyles, aestheticStyles];
+
+const defaultOptions: ComposeOptions<FASTCheckbox, CheckboxStatics> = {
+    statics: {
+        [CheckboxStatics.checked]: checkmarkIcon,
+        [CheckboxStatics.indeterminate]: subtractIcon
+    },
+    styleModules
+}
 
 /**
  * @public
  */
 export function composeCheckbox(
     ds: DesignSystem,
-    options?: ComposeOptions<FASTCheckbox, CheckboxStatics>
+    options: ComposeOptions<FASTCheckbox, CheckboxStatics> = defaultOptions
 ): FASTElementDefinition {
     if (options?.statics) {
         if (!ds.statics.has(CheckboxStatics.checked)) {
--- packages/adaptive-web-components/src/components/checkbox/checkbox.definition.ts
+++ packages/adaptive-web-components/src/components/checkbox/checkbox.definition.ts
@@ -1,25 +1,16 @@
-import checkmarkIcon from "@fluentui/svg-icons/icons/checkmark_16_regular.svg";
-import subtractIcon from "@fluentui/svg-icons/icons/subtract_16_regular.svg";
+import { customElement, type PartialFASTElementDefinition } from '@microsoft/fast-element';
+import { FASTCheckbox } from '@microsoft/fast-foundation';
 import { DefaultDesignSystem } from "../../design-system.js";
 import { composeCheckbox } from './checkbox.compose.js';
-import { CheckboxStatics } from "./checkbox.template.js";
-import { styleModules } from "./checkbox.styles.modules.js";
 
 /**
- * The Checkbox custom element definition. Implements {@link @microsoft/fast-foundation#FASTCheckbox}.
+ * The default Adaptive Checkbox custom element. Implements {@link @microsoft/fast-foundation#FASTCheckbox}.
  *
  * @remarks
  * HTML Element: \<adaptive-checkbox\>
  *
  * @public
  */
-export const checkboxDefinition = composeCheckbox(
-    DefaultDesignSystem,
-    {
-        statics: {
-            [CheckboxStatics.checked]: checkmarkIcon,
-            [CheckboxStatics.indeterminate]: subtractIcon
-        },
-        styleModules,
-    }
-);
+customElement(
+    composeCheckbox(DefaultDesignSystem) as unknown as PartialFASTElementDefinition
+)(FASTCheckbox)
