77409965b0938ee4a90b416559a28ad1d652ac42
Brian Heston <47367562+bheston@users.noreply.github.com>
2023-08-30 13:22:53 -0700

    Review comments

--- packages/adaptive-ui-explorer/src/components/adaptive-component.ts
+++ packages/adaptive-ui-explorer/src/components/adaptive-component.ts
@@ -49,8 +49,6 @@ export class AdaptiveComponent extends FASTElement {
     @observable
     public styles?: Styles;
     protected stylesChanged(prev: Styles, next: Styles) {
-        // console.log("stylesChanged", next);
-        
         // if (prev) {
         //     prev.forEach((s) => this.$fastController.removeStyles(s));
         // }
