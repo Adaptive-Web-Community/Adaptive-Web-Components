9745238a64bf59bff6e717f39a32602f419efb3c
Brian Heston <47367562+bheston@users.noreply.github.com>
2023-10-13 17:40:39 -0700

    Import fix

--- packages/adaptive-ui-figma-designer/src/ui/ui-controller-styles.ts
+++ packages/adaptive-ui-figma-designer/src/ui/ui-controller-styles.ts
@@ -1,8 +1,9 @@
 import { StyleProperty, Styles } from "@adaptive-web/adaptive-ui";
 import { nameToTitle } from "../core/registry/recipes.js";
 import { DesignTokenDefinition } from "../core/registry/design-token-registry.js";
+import { STYLE_REMOVE } from "../core/controller.js";
 import { AppliedDesignToken } from "../core/model.js";
-import { STYLE_REMOVE, UIController } from "./ui-controller.js";
+import { UIController } from "./ui-controller.js";
 
 /**
  * A display definition for a single style module.
