28283531ea0f3eb08bd607b3610d27663beedbb5
Brian Heston <47367562+bheston@users.noreply.github.com>
2024-12-17 13:52:29 -0800

    Updated reference density recipes

--- packages/adaptive-ui/src/reference/density.ts
+++ packages/adaptive-ui/src/reference/density.ts
@@ -4,14 +4,25 @@ import { designUnit, strokeThickness } from "./appearance.js";
 /**
  * @public
  */
+export const densityText = new DensityPaddingAndGapTokenGroup("density.text", 0, 1, 0, 1, designUnit, "0px");
+
+/**
+ * @public
+ */
 export const densityControl = new DensityPaddingAndGapTokenGroup("density.control", 3, 2, 2, 1, designUnit, strokeThickness);
 
 /**
  * @public
+ */
+export const densityControlList = new DensityPaddingAndGapTokenGroup("density.controlList", 1, 0, 1, 0, designUnit, strokeThickness);
+
+/**
+ * @public
+ * @deprecated Use densityControlList
  */
 export const densityItemContainer = new DensityPaddingAndGapTokenGroup("density.itemContainer", 1, 0, 1, 0, designUnit, strokeThickness);
 
 /**
  * @public
  */
-export const densityLayer = new DensityPaddingAndGapTokenGroup("density.layer", 4, 4, 4, 4, designUnit, strokeThickness);
+export const densityLayer = new DensityPaddingAndGapTokenGroup("density.layer", 4, 4, 4, 4, designUnit, "0px");
